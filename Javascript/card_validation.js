d352fdb9f67f40240e688be553dbbbedcf977cba
LunarianDream <jelissasummers@aol.com>
2022-11-29 15:54:32 -0500

    Form Submission Working

--- Javascript/card_validation.js
+++ Javascript/card_validation.js
@@ -24,68 +24,63 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
-const cardValidation = function () {
-    let check_cardholder = cardholder.value;
-    let check_cardnumber = cardnumber.value;
-    let check_month = month.value.trim();
-    let check_year = year.value.trim();
-    let check_CVC = CVC.value.trim();
-    let isValid = true;
+// const cardValidation = function () {
+//     let check_cardholder = cardholder.value;
+//     let check_cardnumber = cardnumber.value;
+//     let check_month = month.value.trim();
+//     let check_year = year.value.trim();
+//     let check_CVC = CVC.value.trim();
+//     let isValid = true;
 
-    if (check_cardholder === '') {
-        setErrorFor(cardholder, `Can't be blank`);
-        isValid = false;
-    } else {
-        isValid
-    };
+//     if (check_cardholder === '') {
+//         setErrorFor(cardholder, `Can't be blank`);
+//         isValid = false;
+//     } else {
+//         isValid
+//     };
 
-    if (check_cardnumber == '') {
-        setErrorFor(cardnumber, `Can't be blank`)
-        isValid = false
-    } else {
-        isValid
-    }
+//     if (check_cardnumber == '') {
+//         setErrorFor(cardnumber, `Can't be blank`)
+//         isValid = false
+//     } else {
+//         isValid
+//     }
 
-    if (check_month == '') {
-        setErrorFor(month, `Can't be blank`)
-        isValid = false
-    } else {
-        isValid
-    }
+//     if (check_month == '') {
+//         setErrorFor(month, `Can't be blank`)
+//         isValid = false
+//     } else {
+//         isValid
+//     }
 
-    if (check_year == '') {
-        setErrorFor(year, `Can't be blank`)
-        isValid = false 
-    } else {
-        isValid
-    }
+//     if (check_year == '') {
+//         setErrorFor(year, `Can't be blank`)
+//         isValid = false 
+//     } else {
+//         isValid
+//     }
 
-    if (check_CVC == '') {
-        setErrorFor(CVC, `Can't be blank`)
-        isValid = false
-    } else {
-        isValid
-    }
-}
+//     if (check_CVC == '') {
+//         setErrorFor(CVC, `Can't be blank`)
+//         isValid = false
+//     } else {
+//         isValid
+//     }
+// }
 
-const setErrorFor = function(field, message) {
+const cardValidation = function(field, message) {
 
     for (let i = 0; i < input.length; i++) {
             if (input[i].value == '') {
                 input[i].style.borderColor = 'var(--inputError)';
-                input[i].nextElementSibling.innerText = message;
-                input[i].nextElementSibling.style.color = 'var(--inputError)'
+                input[i].nextElementSibling.style.display = 'inline';
+                input[i].nextElementSibling.innerText = `Can't be blank`;
+                input[i].nextElementSibling.style.color = 'var(--inputError)';
+                let isValid = false
+            } else {
+                form.submit()
             }
     };
-
-
-    // for (let i = 0; i < small.length; i++) {
-    //     small[i].innerText = message
-    //     small[i].style.color = 'var(--inputError)'
-    // }
-    
-
-    console.log(message)
 };
 
 confirm_button.addEventListener('click', cardValidation);
